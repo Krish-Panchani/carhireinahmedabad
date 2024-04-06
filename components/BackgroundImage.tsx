69d6ac9b7927c77043433ff5eedbd6006b2b671a
Krish-Panchani <krishpanchani1346@gmail.com>
2024-04-06 21:55:38 +0530

    Refactor BackgroundImage component and import statements

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
+import { motion } from "framer-motion";
 import { CurrentSlideData, Data } from "./MySlider";
+import Image from "next/image"; // Import Image from next/image
 
 type Props = {
   transitionData: Data;
@@ -17,24 +19,36 @@ function BackgroundImage({ transitionData, currentSlideData }: Props) {
   return (
     <>
       {isClient && transitionData && (
-        <div className="absolute left-0 top-0 z-10 h-full w-full">
+        <motion.div
+          key={transitionData.img}
+          layoutId={transitionData.img}
+          className="absolute left-0 top-0 z-10 h-full w-full"
+          transition={{
+            opacity: { ease: "linear" },
+            layout: { duration: 0.6 },
+          }}
+        >
           <Image
             alt="Transition Image"
             layout="fill"
             objectFit="cover"
             src={transitionData.img}
           />
-        </div>
+        </motion.div>
       )}
       {isClient && currentSlideData && (
-        <div className="absolute left-0 top-0 h-full w-full">
+        <motion.div
+          key={currentSlideData.data.img + "transition"}
+          className="absolute left-0 top-0 h-full w-full"
+          // style={{ opacity: currentSlideData.data.opacity }}
+        >
           <Image
             alt="Current Image"
             layout="fill"
             objectFit="cover"
             src={currentSlideData.data.img}
           />
-        </div>
+        </motion.div>
       )}
     </>
   );
